96f52aeaa1154efe1491040869161093c6f9a31c
mombash <mombash86@gmail.com>
2024-07-02 09:00:25 +0300

    Added multi-vessel support for the table component. 	modified:   src/App.jsx 	modified:   src/Table.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,64 @@ import MapComponent from './MapComponent';
 import Table from './Table';
 import './App.css';
 
+function getLatestVessels(vesselData) {
+  const latestVessels = {};
+  vesselData.forEach(vessel => {
+    // Assuming vessel.id is the unique identifier for each vessel
+    latestVessels[vessel.id] = vessel;
+  });
+  return Object.values(latestVessels);
+}
+
 const App = () => {
-  const [vesselData, setVesselData] = useState([]);
+  const [vesselData, setVesselData] = useState({});
 
   const handleDataReceived = (data) => {
-    setVesselData((prevData) => [...prevData, data]);
+    const vesselId = data[0].id; // Extract vessel ID from the context field
+    console.log('Received data for vessel:', vesselId);
+    setVesselData((prevData) => ({
+      ...prevData,
+      [vesselId]: prevData[vesselId] ? [...prevData[vesselId], data] : [data]
+    }));
   };
 
-  const latestVesselData = vesselData[vesselData.length - 1];
+  // const latestVesselData = vesselData[vesselData.length - 1];
 
-  console.log(vesselData); // Log the vesselData array
-  if (vesselData.length > 1) {
-    console.log(latestVesselData[0].lat); // Log the latestVesselData
+
+  console.log("Number of Vessels: ", Object.keys(vesselData).length);
+  console.log("Vessel Data Type: ", typeof vesselData);
+  console.log("Vessel Data: ", vesselData);
+  if (Object.keys(vesselData).length) {
+    // console.log("Latest Latitude:", latestVesselData[0].lat);
   }
+
+  console.log("Is Vessel an Object: ", vesselData.constructor === Object);
+  const keys = Object.keys(vesselData);
+  const someKey = keys[0];
+  console.log("Some Key: ", someKey)
+  console.log("Type of Some Key: ", typeof someKey)
+  
+  const flattenedVesselData = Object.values(vesselData).flat(2);
+  const latestVesselData = getLatestVessels(flattenedVesselData);
 
   return (
     <div className="app-container">
       <h1>Vessel Navigation Data</h1>
       <MqttClient onDataReceived={handleDataReceived} />
-      {vesselData.length > 1 ? (
+      {typeof someKey !== 'undefined' ? (
         <>
-          <Table vesselData={latestVesselData} />
-          <MapComponent vesselData={vesselData} latestVesselData={latestVesselData} />
+          {vesselData[someKey].length > 1 ? (
+            <>
+              <Table latestVesselData={latestVesselData} />
+              {/* <MapComponent vesselData={vesselData} latestVesselData={latestVesselData} /> */}
+            </>
+          ) : (
+            <p>Loading vessel data...</p>
+          )}
         </>
       ) : (
-        <p>Loading vessel data...</p>
-      )}
+        <p>No vessel data yet...</p>)
+      }
     </div>
   );
 };
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,28 +1,27 @@
 // Table.jsx
 import React from 'react';
 
-function Table({ vesselData }) { // Accept vesselData as a prop
-
-  return (
-    <table className='table'>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Speed (knots)</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        {vesselData.map((vessel) => (
-          <tr key={vessel.id}>
-            <td>{vessel.id}</td>
-            <td>{vessel.speed}</td>
-            <td>{`${vessel.lat}, ${vessel.lng}`}</td>
+function Table({ latestVesselData }) {
+    return (
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Speed (knots)</th>
+            <th>Location</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+        </thead>
+        <tbody>
+          {latestVesselData.map((vessel) => (
+            <tr key={vessel.id}>
+              <td>{vessel.id}</td>
+              <td>{vessel.speed}</td>
+              <td>{`${vessel.lat}, ${vessel.lng}`}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 
 export default Table;
