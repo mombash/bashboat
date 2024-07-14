413fc893854553b252ff6582cbbc653a6ef0a972
mombash <mombash86@gmail.com>
2024-07-14 13:18:08 +0300

    made the dashboard look much better

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -6,7 +6,7 @@ import "./Dashboard.css";
 function Dashboard({ vesselMeasurements }) {
   return (
     <div className="dashboard">
-      <h2>Vessel Measurements Dashboard</h2>
+      <h2 className="dashboard-title">Vessel Measurements Dashboard</h2>
       <table className="dashboard-table">
         <tbody>
           {Object.entries(vesselMeasurements).map(([id, measurements]) => (
@@ -15,11 +15,13 @@ function Dashboard({ vesselMeasurements }) {
                 <td colSpan={3}>Measurements for Vessel:</td>
               </tr>
               <tr>
-                <td className="dashboard-table-id" colSpan={3}>{id}</td>
+                <td className="dashboard-table-id" colSpan={3}>
+                  {id}
+                </td>
               </tr>
               <tr>
                 <td>
-                  <div>
+                  <div className="dashboard-tile-container">
                     <TileComponent
                       label="Temperature"
                       measurement={measurements.temperature}
@@ -27,16 +29,20 @@ function Dashboard({ vesselMeasurements }) {
                   </div>
                 </td>
                 <td>
+                <div className="dashboard-tile-container">
                   <TileComponent
                     label="Humidity"
                     measurement={measurements.humidity}
                   />
+                </div>
                 </td>
                 <td>
-                  <TileComponent
-                    label="Pressure"
-                    measurement={measurements.pressure}
-                  />
+                    <div className="dashboard-tile-container">
+                        <TileComponent
+                            label="Pressure"
+                            measurement={measurements.pressure}
+                        />
+                    </div>
                 </td>
               </tr>
             </>
